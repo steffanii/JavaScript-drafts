2555b0f1c01408295c4c5baac5a846dcce83dba2
stephanie <stephanienwosumadueke@gmail.com>
2025-02-08 13:59:46 +0100

    Add files via upload

---
+++ JavaScript udemy/loop.js
@@ -0,0 +1,31 @@
+/* console.log(1)
+console.log(2)
+console.log(4)
+console.log(5)
+console.log(6)
+console.log(7)
+console.log(8)
+console.log(9)
+console.log(10) 
+
+
+for(let i=1; i<=10; i++) {
+    console.log(i);
+} 
+
+for(i=0; i<=20; i +=2) {
+    console.log(i) 
+} 
+for(i=100; i>=0; i-=10) {
+    console.log(i)
+} 
+
+animals = ["lion", "Giraffe", "cats", "Dogs" ]
+for(let i=0; i <= animals.length; i++) {
+    console.log(i,  animals[i])
+}*/
+
+animals = ["lion", "Giraffe", "cats", "Dogs" ]
+for(let i= animals.length -1; i>=0 ; i--) {
+    console.log(i, animals[i])
+}
