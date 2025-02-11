ab866c359a6e31e9262d949782e371de3aa21dc9
stephanie <stephanienwosumadueke@gmail.com>
2025-02-11 11:46:53 +0100

    Add files via upload

--- JavaScript udemy/loop.js
+++ JavaScript udemy/loop.js
@@ -23,9 +23,28 @@ for(i=100; i>=0; i-=10) {
 animals = ["lion", "Giraffe", "cats", "Dogs" ]
 for(let i=0; i <= animals.length; i++) {
     console.log(i,  animals[i])
-}*/
+}
 
 animals = ["lion", "Giraffe", "cats", "Dogs" ]
 for(let i= animals.length -1; i>=0 ; i--) {
     console.log(i, animals[i])
 }
+for (let i = 0; i <=10; i++) {
+    console.log(`i is: ${i}`)
+    for (let j = 1; j < 4; j++) {
+        console.log(`   j is: ${j}`)
+    }
+} */
+
+
+ const seatingChart = [
+    ["Kristen", "Erik", "Namita"],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Sakura', 'Jack', "Erika"]
+ ]   
+ for (let i= 0; i < seatingChart.length; i++) {
+    const row = seatingChart[i];
+    console.log(`ROW ${i +1}`)
+    for (let j=0; j < row.length; j++)
+        console.log(row[j])
+ }
